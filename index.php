7c6f114ce1e1d4dbccc240195726e3d707a0fc88
AlexandraCruz <acj159@gmail.com>
2020-04-29 17:36:11 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pais Ciudad</title>
+  
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">  
+ <link rel="stylesheet" href="style.css">  	
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script language="javascript" type="text/javascript">
+
+	function showState(platform_id)
+	{
+		document.frm.submit();
+	}
+		
+</script>
+  </head>
+  <body>
+  <div class="encabezado" style="height:150px"></div>
+      <h1>Pais Ciudad</h1>
+          <?php 
+		   ini_set('display_errors', 1);
+		  include("php/script.php"); ?>
+  </body>
+</html>
